ca230aa0376f1c50d11b5182dc98da97fffe2fe5
Evgeniy <ev_geniy_001@mail.ru>
2019-10-26 14:46:01 +0300

    поправил замечания. вроде все доверстал.

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+// popup обратной связи
 let writeUsLink = document.querySelector(".write-us");
 let writeUsPopup = document.querySelector(".write-us-popup");
 let writeUsForm = writeUsPopup.querySelector(".write-us-form");
@@ -21,7 +22,6 @@ try {
 writeUsLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   writeUsPopup.classList.add("popup-show");
-
   if (storageName) {
     writeUsName.value = storageName;
     writeUsEmail.focus();
@@ -51,6 +51,7 @@ writeUsForm.addEventListener("submit", function (evt) {
   if (!writeUsName.value || !writeUsEmail.value || !writeUsText.value) {
     evt.preventDefault();
     writeUsPopup.classList.remove("popup-error");
+    writeUsPopup.offsetWidth = writeUsPopup.offsetWidth;
     writeUsPopup.classList.add("popup-error");
   } else {
     if (isStorageSupport) {
@@ -67,6 +68,30 @@ window.addEventListener("keydown", function (evt) {
       evt.preventDefault();
       writeUsPopup.classList.remove("popup-show");
       writeUsPopup.classList.remove("popup-error");
+    }
+  }
+});
+
+// popup карты
+let mapLink = document.querySelector(".map-mini");
+let mapPopup = document.querySelector(".map-popup");
+let mapClose = mapPopup.querySelector(".map-close");
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("popup-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("popup-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("popup-show")) {
+      evt.preventDefault();
+      mapPopup.classList.remove("popup-show");
     }
   }
 });
