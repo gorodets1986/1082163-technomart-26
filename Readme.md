963bbc1dae0438022e5c898423f08a3b111b5a6f
Evgeniy <ev_geniy_001@mail.ru>
2019-11-02 22:34:31 +0300

    внес оставшиеся правки по замечаниям проверяющего

--- Readme.md
+++ Readme.md
@@ -20,17 +20,17 @@ _`.editorconfig`, `.gitattributes`, `.gitignore`, `Contributing.md`, `Readme.md`
 
 [Откройте мастер-репозиторий](https://github.com/htmlacademy-htmlcss/1082163-technomart-26) и нажмите кнопку «Fork» в правом верхнем углу. Репозиторий из Академии скопируется в ваш аккаунт.
 
-<img width="769" alt="" src="https://user-images.githubusercontent.com/10909/29037784-cf833fec-7bad-11e7-8eec-dfe32aac11b1.jpg">
+<img width='769' alt='' src='https://user-images.githubusercontent.com/10909/29037784-cf833fec-7bad-11e7-8eec-dfe32aac11b1.jpg'>
 
 Получится вот так:
 
-<img width="769" alt="" src="https://user-images.githubusercontent.com/10909/29037785-d1363f60-7bad-11e7-99da-f02a2f996a01.jpg">
+<img width='769' alt='' src='https://user-images.githubusercontent.com/10909/29037785-d1363f60-7bad-11e7-99da-f02a2f996a01.jpg'>
 
 #### 3. Клонируйте репозиторий на свой компьютер
 
 Будьте внимательны: нужно клонировать свой репозиторий (форк), а не репозиторий Академии. Нажмите кнопку «Clone or download», а затем «Open in Desktop», чтобы клонировать репозиторий через программу [GitHub Desktop](https://desktop.github.com):
 
-<img width="769" alt="" src="https://user-images.githubusercontent.com/10909/29037788-d26a3558-7bad-11e7-9d08-2f9f0f6e467a.jpg">
+<img width='769' alt='' src='https://user-images.githubusercontent.com/10909/29037788-d26a3558-7bad-11e7-9d08-2f9f0f6e467a.jpg'>
 
 Программа клонирует репозиторий на ваш компьютер и подготовит всё необходимое для старта работы.
 
@@ -38,6 +38,6 @@ _`.editorconfig`, `.gitattributes`, `.gitignore`, `Contributing.md`, `Readme.md`
 
 ---
 
-<a href="https://htmlacademy.ru/intensive/htmlcss"><img align="left" width="50" height="50" alt="HTML Academy" src="https://up.htmlacademy.ru/static/img/intensive/htmlcss/logo-for-github-2.png"></a>
+<a href='https://htmlacademy.ru/intensive/htmlcss'><img align='left' width='50' height='50' alt='HTML Academy' src='https://up.htmlacademy.ru/static/img/intensive/htmlcss/logo-for-github-2.png'></a>
 
 Репозиторий создан для обучения на профессиональном онлайн‑курсе «[HTML и CSS, уровень 1](https://htmlacademy.ru/intensive/htmlcss)» от [HTML Academy](https://htmlacademy.ru).
--- js/popup.js
+++ js/popup.js
@@ -1,38 +1,38 @@
 // popup обратной связи
-let writeUsLink = document.querySelector(".write-us");
-let writeUsPopup = document.querySelector(".write-us-popup");
-let writeUsForm = document.querySelector(".write-us-form");
-let closePopup = document.querySelector(".write-us-close");
-let writeUsButton = document.querySelector(".write-us-button button");
-let writeUsName = document.querySelector("[name=name]");
-let writeUsEmail = document.querySelector("[name=email]");
-let writeUsText = document.querySelector("[name=text]");
+let writeUsLink = document.querySelector('.write-us');
+let writeUsPopup = document.querySelector('.write-us-popup');
+let writeUsForm = document.querySelector('.write-us-form');
+let closePopup = document.querySelector('.write-us-close');
+let writeUsButton = document.querySelector('.write-us-button button');
+let writeUsName = document.querySelector('[name=name]');
+let writeUsEmail = document.querySelector('[name=email]');
+let writeUsText = document.querySelector('[name=text]');
 
-let mapLink = document.querySelector(".map-mini");
-let mapPopup = document.querySelector(".map-popup");
-let mapClose = document.querySelector(".map-close");
+let mapLink = document.querySelector('.map-mini');
+let mapPopup = document.querySelector('.map-popup');
+let mapClose = document.querySelector('.map-close');
 
-let productOnLinks = document.querySelectorAll(".catalog-buy-link");
+let productOnLinks = document.querySelectorAll('.catalog-buy-link');
 let productOnLink;
-let productOnPopup = document.querySelector(".product-on-popup");
-let productOnClose = productOnPopup.querySelector(".product-on-close");
+let productOnPopup = document.querySelector('.product-on-popup');
+let productOnClose = productOnPopup.querySelector('.product-on-close');
 let isStorageSupport = true;
-let storageName = "";
-let storageEmail = "";
-let storageText = "";
+let storageName = '';
+let storageEmail = '';
+let storageText = '';
 
 try {
-  storageName = localStorage.getItem("name");
-  storageEmail = localStorage.getItem("email");
-  storageText = localStorage.getItem("text");
+  storageName = localStorage.getItem('name');
+  storageEmail = localStorage.getItem('email');
+  storageText = localStorage.getItem('text');
 } catch (err) {
   isStorageSupport = false;
 }
 
 if (writeUsLink) {
-  writeUsLink.addEventListener("click", function (evt) {
+  writeUsLink.addEventListener('click', function (evt) {
     evt.preventDefault();
-    writeUsPopup.classList.add("popup-show");
+    writeUsPopup.classList.add('popup-show');
     writeUsName.focus();
     if (storageName) {
       writeUsName.value = storageName;
@@ -50,59 +50,59 @@ if (writeUsLink) {
 }
 
 if (closePopup) {
-  closePopup.addEventListener("click", function (evt) {
+  closePopup.addEventListener('click', function (evt) {
     evt.preventDefault();
-    writeUsPopup.classList.remove("popup-show");
-    writeUsPopup.classList.remove("popup-error");
+    writeUsPopup.classList.remove('popup-show');
+    writeUsPopup.classList.remove('popup-error');
   });
 }
 
 if (writeUsForm) {
-  writeUsForm.addEventListener("submit", function (evt) {
+  writeUsForm.addEventListener('submit', function (evt) {
     if (!writeUsName.value || !writeUsEmail.value || !writeUsText.value) {
       evt.preventDefault();
-      writeUsPopup.classList.remove("popup-error");
-      writeUsPopup.classList.add("popup-error");
+      writeUsPopup.classList.remove('popup-error');
+      writeUsPopup.classList.add('popup-error');
     } else {
       if (isStorageSupport) {
-        localStorage.setItem("name", writeUsName.value);
-        localStorage.setItem("email", writeUsEmail.value);
-        localStorage.setItem("text", writeUsText.value);
+        localStorage.setItem('name', writeUsName.value);
+        localStorage.setItem('email', writeUsEmail.value);
+        localStorage.setItem('text', writeUsText.value);
       }
     }
   });
 }
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    if (writeUsPopup.classList.contains("popup-show")) {
+    if (writeUsPopup.classList.contains('popup-show')) {
       evt.preventDefault();
-      writeUsPopup.classList.remove("popup-show");
-      writeUsPopup.classList.remove("popup-error");
+      writeUsPopup.classList.remove('popup-show');
+      writeUsPopup.classList.remove('popup-error');
     }
   }
 });
 
 // popup карты
 if (mapLink) {
-  mapLink.addEventListener("click", function (evt) {
+  mapLink.addEventListener('click', function (evt) {
     evt.preventDefault();
-    mapPopup.classList.add("popup-show");
+    mapPopup.classList.add('popup-show');
   });
 }
 
 if (mapClose) {
-    mapClose.addEventListener("click", function (evt) {
+    mapClose.addEventListener('click', function (evt) {
     evt.preventDefault();
-    mapPopup.classList.remove("popup-show");
+    mapPopup.classList.remove('popup-show');
   });
 }
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    if (mapPopup.classList.contains("popup-show")) {
+    if (mapPopup.classList.contains('popup-show')) {
       evt.preventDefault();
-      mapPopup.classList.remove("popup-show");
+      mapPopup.classList.remove('popup-show');
     }
   }
 });
@@ -110,27 +110,27 @@ window.addEventListener("keydown", function (evt) {
 if (productOnLinks){
   for (let i=0; i < productOnLinks.length; i++) {
     productOnLink = productOnLinks[i];
-    productOnLink.addEventListener("click", popupShow);
+    productOnLink.addEventListener('click', popupShow);
   }
 }
 
 function popupShow(evt) {
   evt.preventDefault();
-  productOnPopup.classList.add("popup-show");
+  productOnPopup.classList.add('popup-show');
 }
 
 if (productOnClose) {
-  productOnClose.addEventListener("click", function (evt) {
+  productOnClose.addEventListener('click', function (evt) {
     evt.preventDefault();
-    productOnPopup.classList.remove("popup-show");
+    productOnPopup.classList.remove('popup-show');
   });
 }
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    if (productOnPopup.classList.contains("popup-show")) {
+    if (productOnPopup.classList.contains('popup-show')) {
       evt.preventDefault();
-      productOnPopup.classList.remove("popup-show");
+      productOnPopup.classList.remove('popup-show');
     }
   }
 });
